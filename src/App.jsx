08f43c233f0a7245613398cbab81d8f703048dc5
Dups aure <duplonhannah08@gmail.com>
2024-12-05 18:43:30 +0800

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Homepage from "./user/Homepage/Homepage.jsx";
 import Login from "./user/loginSignupPage/loginRegister.jsx";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Homepage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
