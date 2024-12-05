08b87b5c6410942e00adba5b68c3721bc0edbb74
V2day7 <octavianolheomark@yahoo.com>
2024-12-05 20:56:29 +0800

    content page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Homepage from "./user/Homepage/Homepage.jsx";
+import Contentpage from "./user/contentPage/contentPage.jsx";
+import commentUser from "./user/contentPage/commentUser.jsx";
+import sheena from "./assets/images/sheena.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Contentpage />
     </>
   );
 }
