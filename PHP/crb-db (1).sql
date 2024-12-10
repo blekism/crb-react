d8c90654312d7c82563a2d7779b77b8acad436c4
blekism <cristianlaviano@gmail.com>
2024-12-10 20:03:30 +0800

    refactor: rename crb-db.sql to crb-db (1).sql and update table structures and data inserts

--- PHP/crb-db (1).sql
+++ PHP/crb-db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2024 at 02:34 PM
+-- Generation Time: Dec 10, 2024 at 01:02 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,6 +45,14 @@ CREATE TABLE `category_id` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `category_id`
+--
+
+INSERT INTO `category_id` (`category_id`, `name`) VALUES
+('1', 'poem'),
+('2', 'short story');
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +65,7 @@ CREATE TABLE `comments_tbl` (
   `user_id` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -73,6 +81,16 @@ CREATE TABLE `genre_post_tbl` (
   `category_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `genre_post_tbl`
+--
+
+INSERT INTO `genre_post_tbl` (`gp_id`, `post_id`, `tag_id`, `category_id`) VALUES
+('GP - 2024-10ad4e0', 'POST - 2024-101d6aa', '2', '1'),
+('GP - 2024-10ad728', 'POST - 2024-101d6aa', '1', '1'),
+('GP - 2024-10ecf72', 'POST - 2024-1042b28', '1', '1'),
+('GP - 2024-10ed08e', 'POST - 2024-1042b28', '2', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +101,14 @@ CREATE TABLE `genre_tbl` (
   `tag_id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genre_tbl`
+--
+
+INSERT INTO `genre_tbl` (`tag_id`, `name`) VALUES
+('1', 'horror'),
+('2', 'comedy');
 
 -- --------------------------------------------------------
 
@@ -95,11 +121,26 @@ CREATE TABLE `posts_tbl` (
   `user_id` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `published_at` varchar(255) NOT NULL,
+  `status` enum('for_review','rejected','posted','reviewing') NOT NULL,
+  `analysis_score` varchar(255) NOT NULL,
+  `published_at` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts_tbl`
+--
+
+INSERT INTO `posts_tbl` (`post_id`, `user_id`, `title`, `content`, `status`, `analysis_score`, `published_at`, `created_at`, `updated_at`) VALUES
+('POST - 2024-09d58ca', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all hated yngwie serrano', 'for_review', 'negative', NULL, '2024-12-09 18:43:24', '0000-00-00 00:00:00'),
+('POST - 2024-09d619e', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-09 18:44:01', '2024-12-09 18:44:01', '0000-00-00 00:00:00'),
+('POST - 2024-10103a2', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-10 17:35:06', '2024-12-10 17:35:06', '0000-00-00 00:00:00'),
+('POST - 2024-101d6aa', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all hated yngwie serrano', 'for_review', 'negative', NULL, '2024-12-10 18:20:36', '0000-00-00 00:00:00'),
+('POST - 2024-1042b28', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-10 17:39:02', '2024-12-10 17:39:02', '0000-00-00 00:00:00'),
+('POST - 2024-104d4d2', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-10 15:58:16', '2024-12-10 15:58:16', '0000-00-00 00:00:00'),
+('POST - 2024-1073e84', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-10 17:38:51', '2024-12-10 17:38:51', '0000-00-00 00:00:00'),
+('POST - 2024-10f380e', 'USER - 2024-08aa9ad', 'the life of yngwie serrano', 'we all loved yngwie serrano', 'posted', 'positive', '2024-12-10 17:33:09', '2024-12-10 17:33:09', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -126,7 +167,7 @@ CREATE TABLE `reports_tbl` (
   `reason` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -138,11 +179,19 @@ CREATE TABLE `reports_tbl` (
 CREATE TABLE `review_tbl` (
   `review_id` varchar(255) NOT NULL,
   `post_id` varchar(255) NOT NULL,
-  `analysis_score` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL,
+  `analysis_score` varchar(255) NOT NULL,
+  `status` enum('for_review','rejected','posted','reviewing') NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `review_tbl`
+--
+
+INSERT INTO `review_tbl` (`review_id`, `post_id`, `analysis_score`, `status`, `created_at`, `updated_at`) VALUES
+('REJECT - 2024-09aca35', 'POST - 2024-09d58ca', 'negative', 'rejected', '2024-12-09 18:43:24', NULL),
+('REJECT - 2024-10aca63', 'POST - 2024-101d6aa', 'negative', 'rejected', '2024-12-10 18:20:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -152,9 +201,11 @@ CREATE TABLE `review_tbl` (
 
 CREATE TABLE `user_tbl` (
   `user_id` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `role` enum('admin','guest','user') NOT NULL,
   `verification_code` int(11) DEFAULT NULL,
   `verified_at` datetime DEFAULT NULL,
@@ -162,6 +213,15 @@ CREATE TABLE `user_tbl` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_tbl`
+--
+
+INSERT INTO `user_tbl` (`user_id`, `username`, `first_name`, `last_name`, `email`, `password`, `role`, `verification_code`, `verified_at`, `verification_expiry`, `created_at`, `updated_at`) VALUES
+('USER - 2024-086146e', 'akosiblek', 'blake', 'laviano', 'reframing_ml@yahoo.com', '202cb962ac59075b964b07152d234b70', 'user', 142108, NULL, 1733660647, '2024-12-08 20:14:07', NULL),
+('USER - 2024-08a3714', 'akosiblek', 'blake', 'laviano', 'reframing_ml@yahoo.com', '202cb962ac59075b964b07152d234b70', 'user', 804905, NULL, 1733660681, '2024-12-08 20:14:41', NULL),
+('USER - 2024-08aa9ad', 'akosiblek', 'blake', 'laviano', 'cristianpogi.laviano@gmail.com', '202cb962ac59075b964b07152d234b70', 'user', NULL, '2024-12-08 15:41:07', NULL, '2024-12-08 15:02:55', NULL);
 
 --
 -- Indexes for dumped tables
